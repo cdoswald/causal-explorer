7c466e621a7db18188da91d93f87bede40113ec1
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2024-11-14 14:05:06 -0500

    fixed bug

--- CausalEx/main.py
+++ CausalEx/main.py
@@ -146,7 +146,7 @@ def train_SAC(args):
             q_optimizer.zero_grad()
             qf_loss.backward()
             q_optimizer.step()
-100000
+
             if global_step % args.policy_frequency == 0:  # TD 3 Delayed update support
                 for _ in range(
                     args.policy_frequency
