e1ed82ce40614136962e5033b765aeafb89d77fb
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2024-12-04 12:27:05 -0500

    added function to save data to HDF5

--- CausalEx/utils.py
+++ CausalEx/utils.py
@@ -1,5 +1,9 @@
+import h5py
+from typing import Dict, List
+
 import cv2
-import torch
+import numpy as np
+
 
 def save_video(frames, save_path, fps=30):
     height, width, layers = frames[0].shape
@@ -9,3 +13,13 @@ def save_video(frames, save_path, fps=30):
     for frame in frames:
         out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
     out.release()
+
+
+def save_dict_to_hdf5(filepath: str, data_dict: Dict[str, List[np.float32]]):
+    with h5py.File(filepath, "a") as file:
+        for key, data in data_dict.items():
+            if key not in file:
+                file.create_dataset(key, data=data, maxshape=(None,), chunks=True)
+            else:
+                file[key].resize((file[key].shape[0] + len(data),))
+                file[key][-len(data):] = data
