d3a8613e5d1c74ba64df41d49b75f4ec4c607361
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2024-12-04 13:52:00 -0500

    added testing to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Set optional flags
+RUN_TESTS=true
+
+# Install package
 pip install .
 
+# Run abbreviated experiments to verify code works as intended 
+# (takes ~40 seconds; main code will automatically run if tests pass)
+if [ $RUN_TESTS ]; then
+    pytest "CausalEx/tests"
+fi
+
+# Run main experiments
 python3 CausalEx/A_main.py
 
