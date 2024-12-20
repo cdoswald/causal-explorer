60d29970957200a5469315b8ca687192780f5852
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2024-12-20 17:09:17 -0500

    refactored bash script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 # Set optional flags
-RUN_TESTS=true
+SKIP_TESTS=false
 
 # Install package
 pip install .
 
 # Run tests to verify code works as intended 
 # (takes ~40 seconds; main code will automatically run if tests pass)
-if [ $RUN_TESTS ]; then
+all_tests_pass=false
+
+if [ $SKIP_TESTS ]; then
+    all_tests_pass=true
+else
     pytest "CausalEx/tests"
+    if [$? -eq 0 ]; then
+        all_tests_pass=true
+    fi
 fi
 
-# Run experiments
-python3 CausalEx/A_main.py
-python3 CausalEx/B_main.py
+# Run main experiments
+if [ $all_tests_pass ]; then
+    python3 CausalEx/A_main.py
+    python3 CausalEx/B_main.py
+else
+    echo "Tests failed--skipping main experiments (can override in run_experiments.sh file)"
+fi
